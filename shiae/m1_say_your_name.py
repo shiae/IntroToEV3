3683f8af64a3d63092c4cddbaecbe01e444fd15a
Allison Shi <shiae@rose-hulman.edu>
2017-12-07 16:50:48 -0500

    m1_say_your_name more done than before

--- shiae/m1_say_your_name.py
+++ shiae/m1_say_your_name.py
@@ -4,8 +4,8 @@ This module is meant to be your first program that you've made for EV3.
   Make the robot say your name and then beep.
   Use the samples to learn the API calls needed (not much code to write)
 
-Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Dave Fisher and Allison Shi.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 # ------------------------------------------------------------------
@@ -17,3 +17,20 @@ Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 #   Feel free to run it, change it, and run it again with any message you like.
 #   This program is simple, but if you can do this, then you have the tools to do FAR more.
 # ------------------------------------------------------------------
+
+import ev3dev.ev3 as ev3
+
+
+def main():
+    name()
+
+
+def name():
+    # ev3.Sound.speak("TIME CUBE 4ce In 1884,  meridian time personnel met in "
+    #                 "Washington to change Earth time. First words said was "
+    #                 "that only 1 day could be used on Earth to not change "
+    #                 "the 1 day bible. So they applied the 1 day and "
+    #                 "ignored  the  other  3 days.").wait()
+    ev3.Sound.speak("Allison").wait()
+
+main()
